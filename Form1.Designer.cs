09cbf0abf674259500734116cda589500129e3ff
qcinbm <124707403+qcinbm@users.noreply.github.com>
2023-02-13 17:02:17 +0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace MP5_TinhvavebieudoHistogramcuamucxam
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.picBoxHinhGoc = new System.Windows.Forms.PictureBox();
+            this.zGHistogram = new ZedGraph.ZedGraphControl();
+            this.picBoxHinhXam = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxHinhGoc)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxHinhXam)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picBoxHinhGoc
+            // 
+            this.picBoxHinhGoc.Location = new System.Drawing.Point(12, 39);
+            this.picBoxHinhGoc.Name = "picBoxHinhGoc";
+            this.picBoxHinhGoc.Size = new System.Drawing.Size(225, 344);
+            this.picBoxHinhGoc.TabIndex = 0;
+            this.picBoxHinhGoc.TabStop = false;
+            // 
+            // zGHistogram
+            // 
+            this.zGHistogram.Location = new System.Drawing.Point(12, 26);
+            this.zGHistogram.Name = "zGHistogram";
+            this.zGHistogram.ScrollGrace = 0D;
+            this.zGHistogram.ScrollMaxX = 0D;
+            this.zGHistogram.ScrollMaxY = 0D;
+            this.zGHistogram.ScrollMaxY2 = 0D;
+            this.zGHistogram.ScrollMinX = 0D;
+            this.zGHistogram.ScrollMinY = 0D;
+            this.zGHistogram.ScrollMinY2 = 0D;
+            this.zGHistogram.Size = new System.Drawing.Size(752, 367);
+            this.zGHistogram.TabIndex = 2;
+            this.zGHistogram.UseExtendedPrintDialog = true;
+            // 
+            // picBoxHinhXam
+            // 
+            this.picBoxHinhXam.Location = new System.Drawing.Point(290, 412);
+            this.picBoxHinhXam.Name = "picBoxHinhXam";
+            this.picBoxHinhXam.Size = new System.Drawing.Size(253, 41);
+            this.picBoxHinhXam.TabIndex = 3;
+            this.picBoxHinhXam.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.picBoxHinhXam);
+            this.Controls.Add(this.zGHistogram);
+            this.Controls.Add(this.picBoxHinhGoc);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxHinhGoc)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxHinhXam)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picBoxHinhGoc;
+        private ZedGraph.ZedGraphControl zGHistogram;
+        private System.Windows.Forms.PictureBox picBoxHinhXam;
+    }
+}
+
