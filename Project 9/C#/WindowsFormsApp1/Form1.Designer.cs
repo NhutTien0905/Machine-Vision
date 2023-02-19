2eb61f89c2d667195323c189566224e69dfea856
Nguyễn Bạch Nhựt Tiến <96219122+NhutTien0905@users.noreply.github.com>
2023-02-19 22:53:06 +0700

    Add files via upload

---
+++ Project 9/C#/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.orgImage = new System.Windows.Forms.PictureBox();
+            this.frameY = new System.Windows.Forms.PictureBox();
+            this.frameX = new System.Windows.Forms.PictureBox();
+            this.xyzImage = new System.Windows.Forms.PictureBox();
+            this.frameZ = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.orgImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.frameY)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.frameX)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.xyzImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.frameZ)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // orgImage
+            // 
+            this.orgImage.Location = new System.Drawing.Point(36, 45);
+            this.orgImage.Name = "orgImage";
+            this.orgImage.Size = new System.Drawing.Size(270, 270);
+            this.orgImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.orgImage.TabIndex = 0;
+            this.orgImage.TabStop = false;
+            // 
+            // frameY
+            // 
+            this.frameY.Location = new System.Drawing.Point(174, 393);
+            this.frameY.Name = "frameY";
+            this.frameY.Size = new System.Drawing.Size(270, 270);
+            this.frameY.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.frameY.TabIndex = 1;
+            this.frameY.TabStop = false;
+            // 
+            // frameX
+            // 
+            this.frameX.Location = new System.Drawing.Point(667, 45);
+            this.frameX.Name = "frameX";
+            this.frameX.Size = new System.Drawing.Size(270, 270);
+            this.frameX.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.frameX.TabIndex = 2;
+            this.frameX.TabStop = false;
+            // 
+            // xyzImage
+            // 
+            this.xyzImage.Location = new System.Drawing.Point(349, 45);
+            this.xyzImage.Name = "xyzImage";
+            this.xyzImage.Size = new System.Drawing.Size(270, 270);
+            this.xyzImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.xyzImage.TabIndex = 3;
+            this.xyzImage.TabStop = false;
+            // 
+            // frameZ
+            // 
+            this.frameZ.Location = new System.Drawing.Point(532, 393);
+            this.frameZ.Name = "frameZ";
+            this.frameZ.Size = new System.Drawing.Size(270, 270);
+            this.frameZ.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.frameZ.TabIndex = 4;
+            this.frameZ.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "org Image";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(529, 360);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(15, 16);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Z";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(171, 360);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(16, 16);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Y";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(664, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(15, 16);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "X";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(346, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 16);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "XYZ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(978, 686);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.frameZ);
+            this.Controls.Add(this.xyzImage);
+            this.Controls.Add(this.frameX);
+            this.Controls.Add(this.frameY);
+            this.Controls.Add(this.orgImage);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.orgImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.frameY)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.frameX)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.xyzImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.frameZ)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox orgImage;
+        private System.Windows.Forms.PictureBox frameY;
+        private System.Windows.Forms.PictureBox frameX;
+        private System.Windows.Forms.PictureBox xyzImage;
+        private System.Windows.Forms.PictureBox frameZ;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Project 9/C#/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Bitmap img = new Bitmap(@"D:\University\Semester_2_2nd\Machine Vision\image\lena.jpg");
+            orgImage.Image = img;
+            (xyzImage.Image, frameX.Image, frameY.Image, frameZ.Image) = convertColor(img);
+        }
+
+        public byte[,] MultiplyMatrix(byte[,] rgb)
+        {
+            double[,] transfer_matrix = new double[3, 3] { { 0.4124564, 0.3575761, 0.1804375 },
+                                                      { 0.2126729, 0.7151522, 0.0721750 },
+                                                      { 0.0193339, 0.1191920, 0.9503041} };
+            byte[,] converted = new byte[3, 1];
+            int w = transfer_matrix.GetLength(0); // row
+            int h = transfer_matrix.GetLength(1); // column
+            for (int i = 0; i < w; i++)
+            {
+                byte xyz = 0;
+                for (int j = 0; j < h; j++)
+                {
+                    xyz += (byte)(transfer_matrix[i,j] * rgb[j, 0]);
+                }
+                converted[i,0] = xyz;
+            }
+            return converted;
+        }
+
+        public Tuple<Bitmap,Bitmap,Bitmap,Bitmap> convertColor(Bitmap img)
+        {
+            Bitmap XYZ = new Bitmap(img.Width, img.Height);
+            Bitmap X = new Bitmap(img.Width, img.Height);
+            Bitmap Y = new Bitmap(img.Width, img.Height);
+            Bitmap Z = new Bitmap(img.Width, img.Height);
+
+            for (int i = 0; i < img.Width; i++)
+            {
+                for (int j = 0; j < img.Height; j++)
+                {
+                    Color pixel = img.GetPixel(i, j);
+                    byte[,] rgb = new byte[3, 1] { { pixel.R }, { pixel.G }, { pixel.B } };
+                    byte[,] xyz = MultiplyMatrix(rgb);
+                    
+                    XYZ.SetPixel(i, j, Color.FromArgb(xyz[0, 0], xyz[1, 0], xyz[2, 0]));
+                    X.SetPixel(i, j, Color.FromArgb(xyz[0, 0], xyz[0, 0], xyz[0, 0]));
+                    Y.SetPixel(i, j, Color.FromArgb(xyz[1, 0], xyz[1, 0], xyz[1, 0]));
+                    Z.SetPixel(i, j, Color.FromArgb(xyz[2, 0], xyz[2, 0], xyz[2, 0]));
+                }
+            }
+            return Tuple.Create(XYZ, X, Y, Z);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
