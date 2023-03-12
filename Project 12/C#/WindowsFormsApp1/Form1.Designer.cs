0cc2d36878356fe319038eab7ef3cdfe3ea452ad
Nguyễn Bạch Nhựt Tiến <96219122+NhutTien0905@users.noreply.github.com>
2023-03-12 22:05:34 +0700

    Add files via upload

---
+++ Project 12/C#/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.orgImg = new System.Windows.Forms.PictureBox();
+            this.shapenedImg = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.orgImg)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.shapenedImg)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // orgImg
+            // 
+            this.orgImg.Location = new System.Drawing.Point(24, 69);
+            this.orgImg.Name = "orgImg";
+            this.orgImg.Size = new System.Drawing.Size(512, 512);
+            this.orgImg.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.orgImg.TabIndex = 0;
+            this.orgImg.TabStop = false;
+            // 
+            // shapenedImg
+            // 
+            this.shapenedImg.Location = new System.Drawing.Point(593, 69);
+            this.shapenedImg.Name = "shapenedImg";
+            this.shapenedImg.Size = new System.Drawing.Size(512, 512);
+            this.shapenedImg.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.shapenedImg.TabIndex = 1;
+            this.shapenedImg.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Image";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(590, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Shapened Image";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1132, 633);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.shapenedImg);
+            this.Controls.Add(this.orgImg);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.orgImg)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.shapenedImg)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox orgImg;
+        private System.Windows.Forms.PictureBox shapenedImg;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Project 12/C#/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Deployment.Internal;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Bitmap img = new Bitmap(@"D:\University\Semester_2_2nd\Machine Vision\image\lena.jpg");
+            orgImg.Image = img;
+            int[,] mask = new int[3, 3]{ { 0,-1,0},
+                                        { -1,4,-1},
+                                        {0,-1,0 } };
+            shapenedImg.Image = convolution(zeroPad(img,1), mask);
+        }
+
+        public Bitmap zeroPad(Bitmap img,int numPad)
+        {
+            int nH = img.Height + 2*numPad;
+            int nW = img.Width + 2*numPad;
+            Bitmap nImg = new Bitmap(nW,nH);
+
+            for (int i = 0; i < nW; i++)
+            {
+                for (int j = 0; j < nH; j++)
+                {
+                    if (i >= numPad && i < img.Width)
+                    {
+                        if (j >= numPad && j < img.Height)
+                        {
+                            nImg.SetPixel(i, j, img.GetPixel(i - numPad, j - numPad));
+                        }                       
+                    }
+                    else
+                    {
+                        nImg.SetPixel(i, j, Color.FromArgb(0, 0, 0));
+                    }
+                }
+            }
+            return nImg;
+        }
+
+        public Bitmap convolution(Bitmap img, int[,]mask) 
+        {
+            int w = img.Width;
+            int h = img.Height;
+            int lenK = mask.GetLength(0);
+            Bitmap nImg = new Bitmap(img.Width,img.Height);
+            for (int i = 0; i < w - lenK; i++)
+            {
+                for (int j = 0; j < h - lenK; j++)
+                {
+                    Color color = img.GetPixel(i, j);
+                    int r = color.R;
+                    int g = color.G;
+                    int b = color.B;
+                    for (int k = 0; k < lenK; k++)
+                    {
+                        for (int t = 0; t < lenK; t++)
+                        {
+                            Color rgb = img.GetPixel(k + i, t + j);
+                            r += rgb.R * mask[k, t];
+                            g += rgb.G * mask[k, t];
+                            b += rgb.B * mask[k, t];
+                        }
+                    }
+                    int red = Math.Max(Math.Min(r, 255), 0);
+                    int green = Math.Max(Math.Min(g, 255), 0);
+                    int blue = Math.Max(Math.Min(b, 255), 0);
+                    nImg.SetPixel(i,j,Color.FromArgb(red, green, blue));
+                }
+            }
+            return nImg;
+        }
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
